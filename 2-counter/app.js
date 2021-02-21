fccf50457975baea04550c57c027ed34400407b9
OlegAba <olegaba.developer@gmail.com>
2021-02-21 18:19:55 -0500

    Implement MathLib in counter page

--- 2-counter/app.js
+++ 2-counter/app.js
@@ -1,36 +1,32 @@
-// set inital value to zero
-let count = 2;
 // select value and buttons
 const value = document.querySelector("#value");
 const btns = document.querySelectorAll(".btn");
 
-console.log(btns)
+const companies = ["Uber", "Lyft", "Google", "Apple", "SpaceX", "Tinder"]
+const animals = ["Aardvark", "Blue Footed Booby", "Flying Dragon", "Giraffe Weevil", "Hammerhead Slug", "Komodo Dragon", "Naked Mole Rat", "Okapi", "Red Panda"]
+const vowels = ["A", "E", "I", "O", "U"]
 
-// for (let i = 0; i < btns.length; i++) {
-//   // btn.addEventListener()
-// }
+generateMathLib = () => {
+    const companyName = companies[Math.floor(Math.random() * companies.length)]
+    const firstLetter = companyName.charAt(0)
+    const companyString = "My company is a" + ((vowels.indexOf(firstLetter) > -1) ? "n " : " ") + companyName + " for " + animals[Math.floor(Math.random() * animals.length)] + "s!"
 
+    return companyString
+};
+
+value.textContent = generateMathLib();
 
 btns.forEach(function (btn) {
   btn.addEventListener("click", function (e) {
     const styles = e.currentTarget.classList;
-    if (styles.contains("decrease")) {
-      count--;
-    } else if (styles.contains("increase")) {
-      count *= 2;
-    } else {
-      count = 2;
-    }
+    const mathLib = generateMathLib();
 
-    if (count > 0) {
-      value.style.color = "green";
+    if (styles.contains("decrease")) {
+      value.textContent = mathLib;
+    } else if (styles.contains("increase")) {
+      value.textContent = mathLib;
+    } else {
+      value.textContent = '';
     }
-    if (count < 0) {
-      value.style.color = "red";
-    }
-    if (count === 0) {
-      value.style.color = "#222";
-    }
-    value.textContent = count;
   });
 });
